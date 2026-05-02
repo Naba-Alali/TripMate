7658e61e4ce226262f0c2e41b5f388f670336820
Naba-Alali <S202268320@kfupm.edu.sa>
2026-05-02 20:38:22 +0300

    urll

--- tripmate-backend/server.js
+++ tripmate-backend/server.js
@@ -14,7 +14,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://tripmate-ctqk.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
